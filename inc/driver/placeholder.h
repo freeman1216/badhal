7d7846cbda2f4ca0bbd3816a5ec9d52e9f85eac2
fufixman <freeman121634@gmail.com>
2025-11-21 13:03:56 +0100

    placeholder for faster implementation

---
+++ inc/driver/placeholder.h
@@ -0,0 +1,22 @@
+#pragma once
+#ifndef BAD_PLACEHOLDER
+#define BAD_PLACEHOLDER
+
+#include <stdint.h>
+
+#include "common.h"
+
+typedef struct {
+
+} PLACEHOLDER_typedef_t;
+
+typedef enum{
+
+}PLACEHOLDER_features_t;
+
+#define PLACEHOLDER_BASE
+
+#define PLACEHOLDER ((PLACEHOLDER_typedef_t *)PLACEHOLDER_BASE)
+
+
+#endif
